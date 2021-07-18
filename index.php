ebdc77af9fded3c656e75fac78fd3920ca7bcc3d
kharismaputri <imamfadhkur1@gmail.com>
2021-07-18 13:56:50 +0700

    penambahan css bootstrap

--- index.php
+++ index.php
@@ -4,14 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>home</title>
 </head>
 <body>
-
-test
-
-
-<!-- yang registrasi hanya admin, pasien tidak boleh registrasi sendiri -->
 
     <h1>Selamat Datang di SMART</h1>
     <p>Sistem Antrian Terapi</p>
